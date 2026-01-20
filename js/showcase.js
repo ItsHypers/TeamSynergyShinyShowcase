b4caf689e56a67d1f498bf4b36257928a18b3c2d
Ryan <ryanathomas013@gmail.com>
2026-01-20 13:57:10 +0000

    shiny data

--- js/showcase.js
+++ js/showcase.js
@@ -205,7 +205,7 @@ async function initShowcase() {
   if (!pageContainer) return;
 
   const JSON_FILE = "./shiny_database.json";
-  const JSON_VERSION = "v26";
+  const JSON_VERSION = "v27";
   let cachedData = null;
 
   const getData = async () => {
