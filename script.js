f9cebefc70999b26621354dd623ac2f3260c8944
Ryan <ryanathomas013@gmail.com>
2026-02-02 19:30:04 +0000

    d

--- script.js
+++ script.js
@@ -109,10 +109,6 @@ const GIF_BASE_PATH = "./pokemon_gifs";
 
 window.ShinyGifLoader = (() => {
   const TIER_JSON_PATH = "./json/tier_pokemon.json";
-<<<<<<< HEAD
-=======
-  const GIF_BASE_PATH = "./pokemon_gifs";
->>>>>>> origin/main
   const VERSION = "v1";
 
   let tierMap = null;
@@ -123,10 +119,6 @@ window.ShinyGifLoader = (() => {
     return tierName.toLowerCase().replace(/\s+/g, "_");
   }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
   async function loadTierData() {
     if (tierMap) return tierMap;
     if (tierPromise) return tierPromise;
@@ -157,10 +149,6 @@ window.ShinyGifLoader = (() => {
     return tierPromise;
   }
 
-<<<<<<< HEAD
-=======
-  
->>>>>>> origin/main
   async function getShinyGifPath(pokemonName) {
     if (!pokemonName) return "";
 
